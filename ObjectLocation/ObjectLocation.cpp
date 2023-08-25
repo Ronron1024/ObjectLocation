05e6322818a233e94a08aea24bace9bd98c67fc8
Ronron Patapon <ronron1024@protonmail.com>
2023-08-25 11:41:13 +0200

    Add project files.

---
+++ ObjectLocation/ObjectLocation.cpp
@@ -0,0 +1,12 @@
+﻿// ObjectLocation.cpp : Defines the entry point for the application.
+//
+
+#include "ObjectLocation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
