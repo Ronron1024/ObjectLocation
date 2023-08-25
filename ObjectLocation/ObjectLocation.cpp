7190ec1272c43f140496c0bce5938deedc725339
Ronron Patapon <ronron1024@protonmail.com>
2023-08-25 15:15:11 +0200

    Hello realsense

--- ObjectLocation/ObjectLocation.cpp
+++ ObjectLocation/ObjectLocation.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	rs2::pipeline p;
+	p.start();
+
+	while (true)
+	{
+		rs2::frameset frames = p.wait_for_frames();
+		rs2::depth_frame depth = frames.get_depth_frame();
+
+		int width = depth.get_width();
+		int height = depth.get_height();
+
+		float dist_to_center = depth.get_distance(width / 2, height / 2);
+
+		std::cout << "Facing object " << dist_to_center << " meters away.\r";
+	}
+
 	return 0;
 }
--- ObjectLocation/ObjectLocation.h
+++ ObjectLocation/ObjectLocation.h
@@ -6,3 +6,4 @@
 #include <iostream>
 
 // TODO: Reference additional headers your program requires here.
+#include <librealsense2/rs.hpp>
