ba296c6ec1da3524ace76d01a22357263e0ed122
Ronron Patapon <ronron1024@protonmail.com>
2023-08-25 15:45:10 +0200

    hello pcl

--- ObjectLocation/ObjectLocation.cpp
+++ ObjectLocation/ObjectLocation.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+
+	cloud.width = 5;
+	cloud.height = 1;
+	cloud.is_dense = false;
+	cloud.resize(cloud.width * cloud.height);
+
+	for (auto& point : cloud)
+	{
+		point.x = 1024 * rand() / (RAND_MAX + 1.0f);
+		point.y = 1024 * rand() / (RAND_MAX + 1.0f);
+		point.z = 1024 * rand() / (RAND_MAX + 1.0f);
+	}
+
+	for (const auto& point : cloud)
+	{
+		std::cerr << "\t" << point.x << "\t\t" << point.y << "\t\t" << point.z << std::endl;
+	}
+
 	return 0;
 }
