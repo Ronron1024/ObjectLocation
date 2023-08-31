dacf6c185403417ef000557d138491cbbcc34b3b
Ronron Patapon <ronron1024@protonmail.com>
2023-09-01 00:39:36 +0200

    Planar segmentation with visualization

--- ObjectLocation/ObjectLocation.cpp
+++ ObjectLocation/ObjectLocation.cpp
@@ -17,6 +17,34 @@ int main()
 	}
 
 	std::cout << *cloud << std::endl;
+
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+
+	pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setDistanceThreshold(0.01);
+	seg.setInputCloud(cloud);
+
+	seg.segment(*inliers, *coefficients);
+
+	if (inliers->indices.size() == 0)
+	{
+		PCL_ERROR("Could not estimate a planar model.");
+		return 1;
+	}
+
+	std::cout << "Model inliers: " << inliers->indices.size() << std::endl;
+
+	std::cout << *coefficients << std::endl;
+
+	std::uint8_t r = 0, g = 255, b = 0;
+	std::uint32_t rgb = (std::uint32_t)r << 16 | (std::uint32_t) g << 8 | (std::uint32_t)b;
+	for (const auto& idx : inliers->indices)
+		cloud->points[idx].rgb = *reinterpret_cast<float*>(&rgb);
+
 	viewer.showCloud(cloud);
 	while (!viewer.wasStopped());
 
--- ObjectLocation/ObjectLocation.h
+++ ObjectLocation/ObjectLocation.h
@@ -9,3 +9,6 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
